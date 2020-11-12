a6137181175e4ffa16b0ca565a38253d398d2c7f
Daniel Marek <xmarek72@stud.fit.vutbr.cz>
2020-11-12 19:19:06 +0100

    added #ifndef because it made some errors without it

--- temp_src/sym_list.h
+++ temp_src/sym_list.h
@@ -6,12 +6,15 @@
 * Kateøina Neprašová			xnepra01
 **/
 
+#ifndef SYM_LIST
+#define SYM_LIST
+
 #include "symtable.h" 
 #include <stdio.h>
 #include <stdbool.h> 
 
 typedef struct sl_elem { /* sym_list element */
-	sym_tab* st_data;    /* table*/
+	struct hast_table* st_data;    /* table*/
 	struct sl_elem* l;   /* pointer to the previous element of the list */
 	struct sl_elem* r;   /* pointer to the next element of the list */
 	bool accessible;     /* variable defining accessibility */
@@ -28,8 +31,8 @@ typedef struct {          /* sym_list */
 void sl_err();
 sym_list* sl_init();
 void sl_dissolve(sym_list*);
-void sl_insert_first(sym_list*, sym_tab*);
-void sl_insert_last(sym_list*, sym_tab*);
+void sl_insert_first(sym_list*, struct hast_table*);
+void sl_insert_last(sym_list*, struct hast_table*);
 void sl_set_act_first(sym_list*);
 void sl_set_act_last(sym_list*);
 void sl_set_next_act(sym_list*);
@@ -37,9 +40,11 @@ void sl_set_prev_act(sym_list*);
 void sl_set_act_naccesible(sym_list*);
 void sl_set_act_accesible(sym_list*);
 bool sl_get_act_accesibility(sym_list*);
-sym_tab* sl_return_act(sym_list*);
+struct hash_table* sl_return_act(sym_list*);
 void sl_del_first(sym_list*);
 void sl_del_last(sym_list*);
 int sl_get_length(sym_list*);
-void sl_rewrite_act(sym_list*, sym_tab*);
-ht_item* sl_search(sym_list* sl, struct lex_unit* lu);
+void sl_rewrite_act(sym_list*, struct hast_table*);
+struct htable_item* sl_search(sym_list* sl, struct lex_unit* lu);
+
+#endif
