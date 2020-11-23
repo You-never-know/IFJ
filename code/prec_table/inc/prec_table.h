aaa455f42346ed959adaa1e865233319f4625651
kate-nepra <katka.nep@gmail.com>
2020-11-23 18:43:15 +0100

    prec_table created

---
+++ code/prec_table/inc/prec_table.h
@@ -0,0 +1,30 @@
+﻿/**
+* Project:
+* Implementace pøekladaèe imperativního jazyka IFJ20
+*
+* Authors:
+* Daniel Marek 					xmarek72
+* Kateřina Neprašová			xnepra01
+**/
+
+#ifndef PREC_TABLE
+#define PREC_TABLE
+
+#include "structs.h"
+
+enum tokens {
+	DOLLAR,
+	L_BRACKET,
+	R_BRACKET,
+	I,
+	PLUS_MINUS,
+	MUL_DIV,
+	COMPARISON,
+	F,
+	COMMA,
+	ERR,
+};
+
+enum tokens merge_event(lex_unit_t*);
+int prec_event(int, int);
+#endif
