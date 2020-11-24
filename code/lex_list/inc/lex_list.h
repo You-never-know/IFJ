6fa588e09b04cdf5e18a0646e23e3c2854b7aada
kate-nepra <katka.nep@gmail.com>
2020-11-24 12:11:05 +0100

    Update lex_list.h

--- code/lex_list/inc/lex_list.h
+++ code/lex_list/inc/lex_list.h
@@ -29,6 +29,8 @@ lex_list* ll_init();
 void ll_dissolve(lex_list*);
 void ll_insert_first(lex_list*, lex_unit_t*);
 void ll_del_first(lex_list*);
+ll_elem_ptr* ll_return_first(lex_list*);
+d_node* ll_return_first_data(lex_list*);
 int ll_get_length(lex_list*);
 
 #endif
