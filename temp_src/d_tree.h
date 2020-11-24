6ec2481a021500828bdcd3e41e07e74f3b76173a
kate-nepra <katka.nep@gmail.com>
2020-11-24 12:30:15 +0100

    Adding d_tree

---
+++ temp_src/d_tree.h
@@ -0,0 +1,22 @@
+#include "structs.h" 
+#ifndef D_TREE
+#define D_TREE
+
+typedef struct d_tree
+{
+	int type; 
+	lex_unit_t * data; //data of derivation tree
+
+	struct d_tree * left;  //left node
+	struct d_tree * right; //right node
+}d_node;
+
+//---------------------------------------------------------------------------- prototypes
+d_node * d_tree_init(d_node * root); // inits d_tree
+d_node * d_root_insert(d_node * root,lex_unit_t * r_data,int r_type); //inserts root
+d_node * d_node_insert_left(d_node * node,lex_unit_t * left_data,int left_type); //insert left son 
+d_node * d_node_insert_right(d_node * node,lex_unit_t * right_data,int right_type); //insert right son
+
+
+
+#endif
--- temp_src/lex_list.h
+++ temp_src/lex_list.h
@@ -10,17 +10,16 @@
 #define LEX_LIST
 
 #include "structs.h" 
+#include "d_tree.h"
 #include <stdio.h>
 
 typedef struct ll_elem { /* lex_list element */
-	lex_unit_t* ll_data;   /* data */
-	struct ll_elem* l;   /* pointer to the previous element of the list */
+	d_node* ll_data;   /* data */
 	struct ll_elem* r;   /* pointer to the next element of the list */
 } *ll_elem_ptr;
 
 typedef struct {          /* lex_list */
-	ll_elem_ptr first;    /* pointer to the first element of the list */
-	ll_elem_ptr last;     /* pointer to the last element of the list */
+	ll_elem_ptr first;    /* pointer to the first element of the list */   
 	int length;	          /* number of elements */
 } lex_list;
 
@@ -30,6 +29,8 @@ lex_list* ll_init();
 void ll_dissolve(lex_list*);
 void ll_insert_first(lex_list*, lex_unit_t*);
 void ll_del_first(lex_list*);
+ll_elem_ptr* ll_return_first(lex_list*);
+d_node* ll_return_first_data(lex_list*);
 int ll_get_length(lex_list*);
 
 #endif
