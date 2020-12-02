782b18e7bf976fb8580a586176caed2ef09468d6
Enexy <enexyges@gmail.com>
2020-12-02 18:42:20 +0100

    prog params type new lines ...

--- code/syntactic_analyzer/src/syntactic_analyzer.c
+++ code/syntactic_analyzer/src/syntactic_analyzer.c
@@ -41,6 +41,166 @@ lex_unit_t * getActiveToken() {
 
 /*****************************************************************  Syntactic functions part **********************************************************************/
 
+bool NL5(lex_unit_t *act){
+
+	if(act==NULL)return true; // end of file
+
+	if(!strcmp(act->data,"package") || //only this should be accepted 
+	   !strcmp(act->data,"func")	||
+	   !strcmp(act->data,"if")){
+		return true;
+	}
+	else
+		return false;
+}
+
+bool NL4(lex_unit_t *act){
+
+	if(act==NULL)return false;
+
+	if(!strcmp(act->data,"\n")){
+		return NL4(getNextToken()); //skipping NEW LINES
+	}
+	else{
+		return NL5(act); //got breakpoint
+	}
+}
+
+bool type(lex_unit_t * act){
+
+	if(act==NULL)return false;
+
+	if(!strcmp(act->data,"int")||   
+	   !strcmp(act->data,"float64")||
+	   !strcmp(act->data,"string"))
+		return true;
+	else
+		return false;
+}
+
+bool par_list(lex_unit_t * act){
+
+	if(act==NULL)return false;
+
+	if(!strcmp(act->data,")"))return true; // end of param list
+	
+	/* next param starts */
+
+	if(strcmp(act->data,","))return false;
+
+	/* id */
+
+	act=getNextToken();
+	if(act==NULL)return false;
+	if(act->unit_type!=IDENTIFICATOR)return false;
+
+	if(!type(getNextToken()))return false;
+
+	return par_list(getNextToken());
+
+
+}
+
+bool par_list_start(lex_unit_t * act){
+
+	if(act==NULL)return false;
+
+	/* no params */
+
+	if(!strcmp(act->data,")"))return true;
+
+	/* id */
+
+	act=getNextToken();
+	if(act==NULL)return false;
+	if(act->unit_type!=IDENTIFICATOR)return false;
+
+	if(!type(getNextToken()))return false;
+
+	return par_list(getNextToken()); // returns true only if finds ")"
+
+
+}
+
+bool params(lex_unit_t * act){
+
+	if(act==NULL)return false;
+
+	/* colum requrired */
+
+	if(strcmp(act->data,"("))return false;
+
+	if(!par_list_start(getNextToken()))return false;
+	
+	act=getActiveToken();
+	
+	if(strcmp(act->data,")"))return false; 
+	else return true;	
+	
+}
+
+bool fun2(lex_unit_t * act){
+
+	if(act==NULL)return true; //end of file
+	
+	/* func required */
+
+	if(strcmp(act->data,"func"))return false;
+
+	/* id required */
+
+	act=getNextToken();
+	if(act==NULL)return false;
+	if(act->unit_type!=IDENTIFICATOR)return false;
+
+	// TO DO:
+	return params(getNextToken());
+
+
+}
+bool prog(){
+
+	/* prog starts */
+
+	lex_unit_t * act = getActiveToken();
+	if(act == NULL)return false;
+
+	/* optional new lines */
+
+	if(!NL4(act))return false; 
+
+	/* first have to package */
+
+	act=getActiveToken();
+	if(act==NULL)return false;
+	if(strcmp(act->data,"package"))return false; 
+
+	/* only main should occur */
+
+	act=getNextToken();
+	if(act==NULL)return false;
+	if(strcmp(act->data,"main"))return false;
+
+	/* new line required */
+
+	act=getNextToken();
+	if(act==NULL)return false;
+	if(strcmp(act->data,"\n"))return false;
+
+	/* optional new lines */
+
+	act = getNextToken();
+	if(act == NULL)return false;
+	if(!NL4(act))return false;
+
+	return fun2(getActiveToken());
+
+
+
+}
+
+
+
 
 
 
@@ -54,7 +214,6 @@ lex_unit_t * getActiveToken() {
 
 bool Check_syntax(token_list * t_list, int * return_value, sym_list * id_tables, sym_tab * function_table) {
 
-	t_list=Active_token;
 
 	if (t_list == NULL || return_value == NULL || id_tables == NULL || function_table == NULL) {
 		if (return_value != NULL) {
