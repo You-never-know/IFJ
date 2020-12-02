ae50800ae3778cc7afb9926be2ca5ebf0eb2fbf0
Daniel Marek <xmarek72@stud.fit.vutbr.cz>
2020-12-03 00:16:27 +0100

    new version

--- code/symtable/src/symtable.c
+++ code/symtable/src/symtable.c
@@ -434,16 +434,10 @@ bool clean_row(ht_item * first, sym_tab *st) {
 		if (to_be_deleted->func != NULL){
 			clean_params(to_be_deleted);
 			clean_return_values(to_be_deleted);
-			if (to_be_deleted->func->func_name != NULL) {
-				LexUnitDelete(to_be_deleted->func->func_name);
-			}
 			free(to_be_deleted->func);
 		}
 
 		if(to_be_deleted->id != NULL){
-			if (to_be_deleted->id->id_name != NULL) {
-				LexUnitDelete(to_be_deleted->id->id_name);
-			}
 			free(to_be_deleted->id);
 		}
 
@@ -454,37 +448,6 @@ bool clean_row(ht_item * first, sym_tab *st) {
 	return true;
 }
 
-
-// help function to clean one row
-// return true if success, false if not
-bool clean_row2(ht_item * first, sym_tab *st) {
-
-	if (first == NULL || st == NULL) {
-		fprintf(stderr, "clean_row failed\n" );
-		return false;
-	}
-
-	for (ht_item * tmp = first; tmp != NULL; ){
-
-		ht_item * to_be_deleted = tmp;
-		tmp = tmp->next;
-
-		if (to_be_deleted->func != NULL){
-			clean_params(to_be_deleted);
-			clean_return_values(to_be_deleted);
-			free(to_be_deleted->func);
-		}
-		else {
-			fprintf(stderr, "clean_row2 failed\n");
-			return false;
-		}
-
-		free(to_be_deleted);
-		st->size = st->size -1;
-	}
-
-	return true;
-}
 
 // remove all items from the table
 // return the number of items for control purpose, -1 if something failed
@@ -508,36 +471,6 @@ int clean_table(sym_tab *st) {
 		}
 
 		if (clean_row(st->ptr[i], st) != true) {
-			fprintf(stderr, "clean_table failed because of clean row\n" );
-			return -1;
-		}
-
-	}
-
-	return st->size;
-
- }
-
-// removes all items from the table
-int clean_function_table(sym_tab *st) {
-	if (st == NULL) {
-		fprintf(stderr, "clean_table failed\n" );
-		return -1;
-	}
-
-	for (size_t i = 0; i < st->arr_size; i++) {
-
-		// no more items left
-		if (st->size == 0) {
-			return 0;
-		}
-
-		// if no items
-		if (st->ptr[i] == NULL) {
-			continue;
-		}
-
-		if (clean_row2(st->ptr[i], st) != true) {
 			fprintf(stderr, "clean_table failed because of clean row\n" );
 			return -1;
 		}
