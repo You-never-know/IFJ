4df51b07a690a0d37451fdedecb1ff9c458f97c0
Enexy <enexyges@gmail.com>
2020-12-04 03:38:46 +0100

    sem analysis updated

--- code/sem_analysis/src/sem_analysis.c
+++ code/sem_analysis/src/sem_analysis.c
@@ -13,67 +13,158 @@ enum lex_units id_type_search(sym_list * list_of_tables,lex_unit_t * name){
 	}
 }
 
+Func * func_search(sym_tab * main,lex_unit_t *name){
+
+	ht_item *act=find_item(main,name);
+	
+	if(act==NULL){ // item is not defined or accesible 
+		return ERROR; 
+	}
+	else{
+		if(act->func==NULL)return ERROR;
+		return act->func;
+	}
+
+}
+
+bool op(void* data){
+
+	if(data==NULL){
+		fprintf(stderr,"data eq NULL");
+		return false;
+	}
+	return 	  (!strcmp(data,"+")  || !strcmp(data,"-") ||
+			   !strcmp(data,"/")  || !strcmp(data,"*"));
+}
+
+bool data_type(enum lex_units type){
+
+	return 	  (type == INTEGER 		 ||
+			   type == STRING 		 ||
+			   type == DECIMAL);
+}
+
+bool relational_op(void *data){
+
+	if(data==NULL){
+		fprintf(stderr,"data eq NULL");
+		return false;
+	}
+
+	return 	   (!strcmp(data,"<")  || !strcmp(data,">") ||
+			    !strcmp(data,"==") || !strcmp(data,"!=")||
+			    !strcmp(data,">=") || !strcmp(data,"<="));
+}
 
 
 enum lex_units tree_check(d_node * rh_node,sym_list * list_of_tables){
 
 	if(rh_node->left==NULL && rh_node->right==NULL){
-		if(rh_node->type==EN)
+		
+		if(rh_node->data->unit_type==IDENTIFICATOR)
 			return id_type_search(list_of_tables,rh_node->data);
-		else return ERROR;
+		
+		if(data_type(rh_node->data->unit_type))
+			return rh_node->data->unit_type;
+
+		/* tree is build incorectly */
+		return ERROR;
 	}
 	
 
 	if(rh_node->right!=NULL && rh_node->left!=NULL){
 		
-		if(rh_node->right->type==OP && rh_node->left->type==OP){
-			return ERROR; // tree is build incorectly
-		}
-		if(rh_node->right->type==EN && rh_node->left->type==OP){ //left side OP right side ID
-			
-			if(id_type_search(list_of_tables,rh_node->right->data)== 
-				tree_check(rh_node->left,list_of_tables)) 
-				return id_type_search(list_of_tables,rh_node->right->right->data);
-			else
-				return ERROR;
+		if(op(rh_node->right->data->data) && op(rh_node->left->data->data)){
+			/* tree is build incorectly */
+			return ERROR; 
 		}
 
-		if(rh_node->left->type==EN && rh_node->right->type==OP){ //left side ID right side OP
+		if(rh_node->right->data->unit_type==IDENTIFICATOR && op(rh_node->left->data->data)){ //left side OP right side ID
+
+			if(id_type_search(list_of_tables,rh_node->right->data)==tree_check(rh_node->left,list_of_tables)) 
+					return id_type_search(list_of_tables,rh_node->right->right->data);
+
+			/* err data type compatibility */
+			return ERROR;
+		}
+
+		if(data_type(rh_node->right->data->unit_type) && op(rh_node->left->data->data)){
+
+			if((enum lex_units)rh_node->right->data->unit_type==tree_check(rh_node->left,list_of_tables))
+				return rh_node->right->data->unit_type;
+
+			/* err data type compatibility */
+			return ERROR;
+		}
+
+		if(op(rh_node->right->data->data) && rh_node->left->data->unit_type==IDENTIFICATOR){ //left side ID right side OP
 			
 			if(id_type_search(list_of_tables,rh_node->left->data)==
 				tree_check(rh_node->right,list_of_tables)) 
 				return id_type_search(list_of_tables,rh_node->left->data);
-			else
-				return ERROR;
+
+			/* err data type compatibility */
+			return ERROR;
 
 		}
-		if(rh_node->left->type==EN && rh_node->right->type==EN){ //both sides ID
+
+		if(op(rh_node->right->data->data) && data_type(rh_node->left->data->unit_type)){
+
+			if((enum lex_units)rh_node->left->data->unit_type==tree_check(rh_node->right,list_of_tables))
+				return rh_node->left->data->unit_type;
+
+			/* err data type compatibility */
+			return ERROR;
+		}
+
+
+		if(rh_node->left->data->unit_type==IDENTIFICATOR && rh_node->right->data->unit_type==IDENTIFICATOR){ //both sides ID
+			
 			if(id_type_search(list_of_tables,rh_node->left->data)==
 				id_type_search(list_of_tables,rh_node->right->data))
 				return id_type_search(list_of_tables,rh_node->right->data);
-			else
-				return ERROR;
+
+			/* err data type compatibility */
+			return ERROR;
+		}
+
+		if(data_type(rh_node->left->data->unit_type) && data_type(rh_node->right->data->unit_type)){
+
+			if(rh_node->left->data->unit_type==rh_node->right->data->unit_type)
+				return rh_node->right->data->unit_type;
+
+			/* err data type compatibility */
+			return ERROR;
 		}
 	}
 
 	if(rh_node->right==NULL && rh_node->left!=NULL){
 
-		if(rh_node->left->type==EN)
+		if(rh_node->left->data->unit_type==IDENTIFICATOR)
 			return id_type_search(list_of_tables,rh_node->left->data);
-		else 
-			return ERROR; // tree is build incorrectly
+
+		if(data_type(rh_node->left->data->unit_type))
+			return rh_node->left->data->unit_type;
+
+		/* err data type compatibility */
+		return ERROR; 
 				
 	}
 	if(rh_node->left==NULL && rh_node->right!=NULL){
 
-		if(rh_node->right->type==EN) //have to be ID 
+		if(rh_node->right->data->unit_type==IDENTIFICATOR)
 			return id_type_search(list_of_tables,rh_node->right->data);
-		else 
-			return ERROR; // tree is build incorrectly
+
+		if(data_type(rh_node->right->data->unit_type))
+			return rh_node->right->data->unit_type;
+
+		/* err data type compatibility */
+		return ERROR; 
 				
 	}
 
-	return ERROR; // this should not happen
+	/* this should not happen */
+	return ERROR; 
 
 }
 
@@ -83,20 +174,33 @@ unsigned Sem_analysis(d_node * node,sym_tab * main,sym_list * list_of_tables,lex
 
 	if(node->data==NULL)return SYSTEM_ERROR;
 
-	if(node->data->unit_type!=ASSIGNMENT)return SYSTEM_ERROR; // checks if root is =
-
 	if(node->right==NULL)return SYSTEM_ERROR; // no id or value to assign
 
 	if(list_of_tables==NULL)return SYSTEM_ERROR; // no comment ;)
 
+	if(!strcmp(node->data->data,"=") || !strcmp(node->data->data,"if")){
 
-	enum lex_units right_sd=tree_check(node->right,list_of_tables); // chceck derivation tree
+		if(!strcmp(node->data->data,"if")){
+			if(relational_op(node->right->data->data)) // must be relational operator
+				return COMPATIBLE_ERR;
+		}
 
-	for(d_node * tmp=node->left;tmp!=NULL;tmp=next_left(tmp)){ //pushing left side of tree
-		if(id_type_search(list_of_tables,tmp->data)!=right_sd) // comp data types 
-			return COMPATIBLE_ERR;
+		if(!strcmp(node->data->data,"=")){
+			if(!relational_op(node->right->data->data)) // can not be relational operator
+				return COMPATIBLE_ERR;
+		}
+
+		enum lex_units right_sd=tree_check(node->right,list_of_tables); // chceck derivation tree
+		
+		for(d_node * tmp=node->left;tmp!=NULL;tmp=next_left(tmp)){ //pushing left side of tree		
+			if(id_type_search(list_of_tables,tmp->data)!=right_sd) // comp data types 
+				return COMPATIBLE_ERR;
+		}
+
+		return SEM_PASSED; // should be ok
 	}
 
-	return SEM_PASSED; // should be ok
+return SEM_PASSED;
+
  	
 }
